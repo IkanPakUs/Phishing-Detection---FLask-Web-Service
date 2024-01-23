2e5a535f9eb6d69228a5b9d031333ac1cc422818
IkanPakUs <komanggede86@gmail.com>
2024-01-23 16:36:13 +0800

    exclude func

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, resources={"/api/*": {"origins": "*"}})
-    
-    from app.classify import bp as clasify_bp
-    app.register_blueprint(clasify_bp, url_prefix='/api/classify')
-    
-    if __name__ == '__main__':
-        app.run()
+app = Flask(__name__)
+CORS(app, resources={"/api/*": {"origins": "*"}})
+
+from app.classify import bp as clasify_bp
+app.register_blueprint(clasify_bp, url_prefix='/api/classify')
+
+if __name__ == '__main__':
+    app.run()
